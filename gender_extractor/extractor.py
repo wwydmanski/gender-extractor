bd53c3ef62fa5c7e0bf38ff66285e192a8155d8f
Witold Wydmański <wwydmanski@gmail.com>
2021-07-12 17:32:38 +0200

    feat: add in-betweens

--- gender_extractor/extractor.py
+++ gender_extractor/extractor.py
@@ -7,10 +7,16 @@ import pkgutil
 #%%
 class GenderExtractor:
     def __init__(self):
+        """ Initializes the data.
+
+        If it's the first time that this package is run, creates an index and saves it as pickle.
+        Otherwise, it reads the premade file.
+        """
+
         self.countries_encoding = {}
         self.names_lists = pkgutil.get_data(__name__, "nameLists/list.txt").decode().split(',')
         for fname in self.names_lists:
-            text = os.path.split(fname)[-1]
+            text = os.path.split(fname.replace("\\", "/"))[-1]
             split = re.sub('([A-Z][a-z]+)', r' \1', re.sub('([A-Z]+)', r' \1', text)).split()
             country = split[0].lower()
             self.countries_encoding[country] = len(self.countries_encoding)-1
@@ -23,10 +29,10 @@ class GenderExtractor:
             self._create_pickle()
     
     def _create_pickle(self):
+        """ Creates the index and saves it """
         self.name_freq = {}
-
         for fname in self.names_lists:
-            fname = fname.strip()
+            fname = fname.strip().replace("\\", "/")
             text = os.path.split(fname)[-1]
             split = re.sub('([A-Z][a-z]+)', r' \1', re.sub('([A-Z]+)', r' \1', text)).split()
             country = split[0].lower()
@@ -56,20 +62,37 @@ class GenderExtractor:
         with open(save_loc+"/data.pickle", "wb") as f:
             pickle.dump(self.name_freq, f)
 
-    def extract(self, name, country=None):
+    def extract_gender(self, name, country=None):
+        """ Extracts the suspected gender from the first name of a person.
+
+        Args:
+            name (str): First name
+            country (str, optional): Country that we're focusing. If none selected,
+                the result will follow a general statistic.
+
+        Returns:
+            str: One of [male, mostly male, ambiguous, mostly female, female]
+        """
         name = name.lower()
         try:
-            m_count = self.name_freq[name][0][self.countries_encoding[country.lower()]]
-            f_count = self.name_freq[name][1][self.countries_encoding[country.lower()]]
-        except (KeyError, AttributeError):
-            m_count = sum(self.name_freq[name][0])
-            f_count = sum(self.name_freq[name][1])
+            try:
+                m_count = self.name_freq[name][0][self.countries_encoding[country.lower()]]
+                f_count = self.name_freq[name][1][self.countries_encoding[country.lower()]]
+            except (KeyError, AttributeError):
+                m_count = sum(self.name_freq[name][0])
+                f_count = sum(self.name_freq[name][1])
 
-        if f_count > m_count:
-            return "female"
-        elif m_count > f_count:
-            return "male"
-        else:
+            if f_count/m_count > 0.9:
+                return "female"
+            elif f_count/m_count > 0.6:
+                return "mostly female"
+            elif f_count/m_count < 0.4:
+                return "mostly male"
+            elif f_count/m_count < 0.1:
+                return "male"
+            else:
+                return "ambiguous"
+        except KeyError:
             return "ambiguous"
 
 if __name__=="__main__":
--- tests/basic_test.py
+++ tests/basic_test.py
@@ -3,8 +3,9 @@ from gender_extractor import GenderExtractor
 
 def test_andrea():
     ext = GenderExtractor()
-    assert ext.extract("Andrea")=='female'
-    assert ext.extract("Andrea", "Italy")=='male'
+    assert ext.extract_gender("Andrea")=='female'
+    assert ext.extract_gender("Andrea", "Italy")=='mostly male'
+
 
 if __name__=="__main__":
     test_andrea()
